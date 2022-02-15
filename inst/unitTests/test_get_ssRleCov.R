0b9909e347acc003b913711de4114de59a4b8872
Liu <haibo.liu@umassmed.edu>
2022-02-15 15:23:31 -0500

    avoid adjusting annotated proximal APA

--- inst/unitTests/test_get_ssRleCov.R
+++ inst/unitTests/test_get_ssRleCov.R
@@ -3,28 +3,37 @@ test_get_ssRleCov <- function() {
   start <- seq(1, 9, by = 2)
   end <- seq(2, 10, by = 2)
   testGR <- GRanges("chr1", IRanges(start, end),
-                    strand = "+", score = 1:5)
+    strand = "+", score = 1:5
+  )
   filename <- tempfile()
   export(testGR, filename, format = "BEDGraph")
-  
+
   tag <- c("test")
-  metadata <- data.frame(tag = tag, 
-                         condition = "test",
-                         bedgraph_file = filename)
-  outdir = tempdir()
-  write.table(metadata, file =file.path(outdir, "metadata.txt"), 
-              sep = "\t", quote = FALSE, row.names = FALSE)
-  
-  sqlite_db <- setup_sqlitedb(metadata = file.path(outdir, "metadata.txt"),
-                              outdir)
+  metadata <- data.frame(
+    tag = tag,
+    condition = "test",
+    bedgraph_file = filename
+  )
+  outdir <- tempdir()
+  write.table(metadata,
+    file = file.path(outdir, "metadata.txt"),
+    sep = "\t", quote = FALSE, row.names = FALSE
+  )
+
+  sqlite_db <- setup_sqlitedb(
+    metadata = file.path(outdir, "metadata.txt"),
+    outdir
+  )
   addLockName(filename = tempfile())
-  
-  cov <- get_ssRleCov(bedgraph = filename,
-                      tag = "test",
-                      genome = BSgenome.Mmusculus.UCSC.mm10,
-                      sqlite_db = sqlite_db,
-                      outdir = outdir,
-                      chr2exclude = NULL)
+
+  cov <- get_ssRleCov(
+    bedgraph = filename,
+    tag = "test",
+    genome = BSgenome.Mmusculus.UCSC.mm10,
+    sqlite_db = sqlite_db,
+    outdir = outdir,
+    chr2exclude = NULL
+  )
   cov <- readRDS(cov$coverage_file[cov$tag == "test" & cov$chr == "chr1"])
   checkEquals(rep(2, 5), runLength(cov)[1:5])
   checkEquals(1:5, runValue(cov)[1:5])
--- tests/runTests.R
+++ tests/runTests.R
@@ -1,12 +1,14 @@
-pkgs_required <- c("BSgenome.Mmusculus.UCSC.mm10",
-                   "TxDb.Mmusculus.UCSC.mm10.knownGene",
-                   "EnsDb.Mmusculus.v79",
-                   "rtracklayer",
-                   "GenomicRanges",
-                   "limma", "future.apply")
+pkgs_required <- c(
+  "BSgenome.Mmusculus.UCSC.mm10",
+  "TxDb.Mmusculus.UCSC.mm10.knownGene",
+  "EnsDb.Mmusculus.v79",
+  "rtracklayer",
+  "GenomicRanges",
+  "limma", "future.apply"
+)
 for (pkg in pkgs_required)
 {
-    require(pkg, character.only = TRUE) || stop(pkg, " can't be loaded!")
+  require(pkg, character.only = TRUE) || stop(pkg, " can't be loaded!")
 }
 
 BiocGenerics:::testPackage("InPAS")
